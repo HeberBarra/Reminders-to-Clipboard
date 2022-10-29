9c926b597a4f15dc40f89d407597045e6389e661
Heber F Barra <heberferrirabarra@gmail.com>
2022-10-29 18:53:17 -0300

    Program code logic change

--- getDateInterval.py
+++ getDateInterval.py
@@ -3,8 +3,11 @@ from Reminders import Reminders_Functions
 def main():
     first_date = input('Digite a primeira data: '.strip())
     second_date = input('Digite a segunda data: '.strip())
-    print(Reminders_Functions.date_interval(first_date, second_date))
-
+    dates = Reminders_Functions.date_interval(first_date, second_date)
+    print('[', end='')
+    for date in dates:
+        print(f'"{date}", ' if date != dates[-1] else f'"{date}"', end='')
+    print(']')
 
 if __name__ == '__main__':
     main()
