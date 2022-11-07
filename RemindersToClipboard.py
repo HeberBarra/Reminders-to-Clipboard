8c65011a4327e1fe9bf9f40359b4a1c0c7a050a8
Heber F Barra <heberferrirabarra@gmail.com>
2022-11-07 09:17:20 -0300

    code refactor and upgrade due to package upgrade

--- RemindersToClipboard.py
+++ RemindersToClipboard.py
@@ -1,39 +1,37 @@
-#! python3
-# RemindersToClipboard.py - analyses a JSON file and then copy to clipboard every message that was meant to be send today or to a specific date (day/month/year | 00/00/00)
-
-import time
+from Reminders import genericReminderFunctions
+from Reminders import sendReminder
 import pyperclip
-from Reminders import Reminders_Functions
 
-JSON_FILE = 'reminders.json'
-XLSX_FILE = 'Horário.xlsx'
+
+config_data = genericReminderFunctions.readJsonFile('config.json')
+JSON_FILE = config_data['remindersJsonFilePath']
+XLSX_FILE = config_data['scheduleXlsxFilePath']
+CONTACT_COORDINATES = config_data['contactCoordinates']
+MESSAGE_FIELD_COORDINATES = config_data['messageFieldCoordinates']
+COLOR = config_data['color']
+BROWSER_NAME = config_data['browserName']
+BROWSER_PATH = config_data['browserPath']
 
 
 def main():
-    # Register the starting time
-    start = time.perf_counter()
-
-    # Gets the date and weekday
-    program_date, weekday = Reminders_Functions.get_date()  
-
-    # Saves the original clipboard content so I don't lose any important thing that was there, well it only works with strings/texts
+    program_date, weekday  = genericReminderFunctions.getDate()
     original_clipboard_content = pyperclip.paste()
+    reminders = sendReminder.getScheduleFromFile(XLSX_FILE, weekday) + \
+    '\n' + sendReminder.getRemindersFromJson(JSON_FILE, program_date)
+    print(f'Preview: \n{reminders}')
+    pyperclip.copy(reminders)
 
-    # Creates the message string and sends it to through whatsapp web
-    spreadsheet = Reminders_Functions.read_sheet_from_xlsx_file(XLSX_FILE)
-    message = [Reminders_Functions.create_today_schedule_message(spreadsheet, weekday)]
-    message.append(Reminders_Functions.get_reminders_from_json(JSON_FILE, program_date))
-    message = '\n'.join(message)
+    restoreContent = sendReminder.sendMessage(
+        CONTACT_COORDINATES,
+        MESSAGE_FIELD_COORDINATES,
+        COLOR,
+        BROWSER_PATH,
+        BROWSER_NAME
+    )
 
-    print(f'Preview: \n{message}')
-    pyperclip.copy(message)
-    Reminders_Functions.send_message(24, 399, 638, 738)
+    if restoreContent:
+        pyperclip.copy(original_clipboard_content)
 
-    # Restores the original clipboard content to my clipboard
-    pyperclip.copy(original_clipboard_content)
-
-    # Prints out how many seconds it took to run all the program
-    print(f'\n{time.perf_counter() - start = :.2f}')
 
 
 if __name__ == '__main__':
