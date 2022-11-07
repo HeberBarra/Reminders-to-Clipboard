05811b9399b5a11c4d2518319729645e450f0d00
Heber F Barra <heberferrirabarra@gmail.com>
2022-11-07 09:14:31 -0300

    code refactor due to package upgrade

--- addNewReminder.py
+++ addNewReminder.py
@@ -1,51 +1,31 @@
-from Reminders import Reminders_Functions
+from Reminders import genericReminderFunctions
+from Reminders import showReminder
+from Reminders import createReminder
 import json
 
-JSON_FILE = 'reminders.json' 
-json_lines = Reminders_Functions.read_json_file(JSON_FILE)
-
-
-def isOptionRight(message: str):
-    while True:
-        chosenOption = input(message)
-        isRight = input('A escolha está correta?: ').lower()[0]
-
-        if isRight in 'ysapc':
-            return chosenOption 
-
-
-def chooseSection() -> int:
-    for index in range(len(json_lines)):
-        print(f'[{index}] {Reminders_Functions.remove_title_special_parts(json_lines[index]["Title"])}')
-    while True:
-        chosen_section = int(isOptionRight('Escolha um seção: '))
-        if chosen_section in [number for number in range(len(json_lines))]:
-            return chosen_section
-        
-        print('Digite uma seção válida')
-
-
-def showSection(index: int, file: list) -> None:
-    for item in file[index]['Messages']:
-        print(item['dates'])
-        print(rf"{item['message']}", '\n')
+config_data = genericReminderFunctions.readJsonFile('config.json')
+JSON_FILE = config_data['remindersJsonFilePath']
+json_data = genericReminderFunctions.readJsonFile(JSON_FILE)
 
 
 def main():
-    chosen_section = chooseSection()
-    showSection(chosen_section, json_lines)
-    
-    # Reminder info
-    start_date = isOptionRight('Qual a primeira data?: ')
-    end_date = isOptionRight('Qual a última data?: ')
-    new_message = isOptionRight('Qual a nova messagem?: ')
-    date_interval = Reminders_Functions.date_interval(start_date, end_date)
+    chosen_section = genericReminderFunctions.chooseSection(json_data)
+    showReminder.showSection(chosen_section, json_data)
 
-    # Add reminder to json
-    json_lines[chosen_section]["Messages"].append({"dates": date_interval, "message": new_message.replace('\\n', '\n').replace('\\t', '\t')})
+    start_date = genericReminderFunctions.isOptionRight('Qual a primeira data?: ', 'A escolha está correta?: ')
+    end_date = genericReminderFunctions.isOptionRight('Qual a última data?: ',  'A escolha está correta?: ')
+    new_message = genericReminderFunctions.isOptionRight('Qual a nova messagem?: ',  'A escolha está correta?: ')
+    date_interval = createReminder.getDateInterval(start_date, end_date)
+    json_data[chosen_section]["Messages"].append(
+        {
+            "dates": date_interval, 
+            "message": new_message.replace('\\n', '\n').replace('\\t', '\t')
+        }
+    )
 
     with open(JSON_FILE, 'w', encoding='utf-8') as j_file:
-        json.dump(json_lines, fp=j_file, ensure_ascii=False)
+        json.dump(json_data, fp=j_file, ensure_ascii=False)
+
 
 if __name__ == '__main__':
     main()
--- getDateInterval.py
+++ getDateInterval.py
@@ -1,9 +1,9 @@
-from Reminders import Reminders_Functions
+from Reminders import createReminder
 
 def main():
     first_date = input('Digite a primeira data: '.strip())
     second_date = input('Digite a segunda data: '.strip())
-    dates = Reminders_Functions.date_interval(first_date, second_date)
+    dates = createReminder.getDateInterval(first_date, second_date)
     print('[', end='')
     for date in dates:
         print(f'"{date}", ' if date != dates[-1] else f'"{date}"', end='')
