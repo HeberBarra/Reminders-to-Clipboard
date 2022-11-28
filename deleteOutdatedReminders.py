0d6c4d416db69acace2a94f6c7e0dcd51a35ec2b
Heber F Barra <heberferrirabarra@gmail.com>
2022-11-27 22:31:16 -0300

    Corrected a date format error that caused all non 'ALWAYS' reminders to be deleted

--- deleteOutdatedReminders.py
+++ deleteOutdatedReminders.py
@@ -2,7 +2,8 @@ from Reminders import genericReminderFunctions
 from Reminders import deleteReminder
 import json
 
-DATE = genericReminderFunctions.getDate()[0]
+# Gets a dd/mm/yy date and converts to a dd/mm format
+DATE = '/'.join(genericReminderFunctions.getDate()[0].split('/')[:2])
 CONFIG_FILE = 'config.json'
 CONFIG_DATA = genericReminderFunctions.readJsonFile(CONFIG_FILE)
 JSON_FILE = CONFIG_DATA['remindersJsonFilePath']
@@ -11,7 +12,7 @@ JSON_FILE = CONFIG_DATA['remindersJsonFilePath']
 def main():
     json_data = genericReminderFunctions.readJsonFile(JSON_FILE)
     remaining_reminders = deleteReminder.getUndeletedRemindersData(json_data, DATE)
-
+    print(remaining_reminders)
     with open(JSON_FILE, 'w+', encoding='UTF-8') as json_file:
         json.dump(remaining_reminders, fp=json_file, ensure_ascii=False)
 
