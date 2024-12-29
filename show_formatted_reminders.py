3fec6adbd22a880f5cd4f4fae6360d51dae0fd6a
Heber F Barra <heberferreirabarra@gmail.com>
2024-12-29 14:04:01 -0300

    [ REFACTOR ] Mudança no nome do arquivo de camelCase para snake_case

--- show_formatted_reminders.py
+++ show_formatted_reminders.py
@@ -4,11 +4,9 @@ from reminders import reminder_json
 CONFIG_FILE = 'config.json'
 CONFIG_DATA = json_utils.read_json_file(CONFIG_FILE)
 JSON_FILE = CONFIG_DATA['remindersJsonFilePath']
-
+json_data = json_utils.read_json_file(JSON_FILE)
 
 def main():
-    json_data = json_utils.read_json_file(JSON_FILE)
-
     for section in json_data:
         reminder_json.print_section(section, section == json_data[-1])
 
