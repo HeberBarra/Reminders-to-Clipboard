88f34722d4bb3b8d37d2900a5f1d76f8e55d3690
Heber F Barra <heberferreirabarra@gmail.com>
2025-01-01 22:06:30 -0300

    [ REFACTOR ] Removed now useless functions

--- reminders/reminder_json.py
+++ reminders/reminder_json.py
@@ -1,12 +1,6 @@
 from os import stat, path
 
 
-def show_section(index: int, json_data: dict) -> None:
-    for item in json_data['reminders'][index]['Messages']:
-        print(item['dates'])
-        print(f'{item['message']}\n')
-
-
 def format_title(title: str) -> str:
     special_parts = ['\n', '*', '_', ':']
 
@@ -14,36 +8,6 @@ def format_title(title: str) -> str:
         title = title.replace(part, '')
 
     return title
-
-
-def print_reminder(reminder, last=False):
-    if not reminder:
-        return
-
-    print(
-        f"""    {{
-      "dates": {reminder['dates']},
-      "message": "{reminder['message']}" 
-    }}{',' if not last else ''}
-        """,
-        end='',
-    )
-
-
-def print_reminders(section: dict) -> None:
-    for reminder in section['Messages']:
-        is_last = reminder == section['Messages'][-1]
-        print_reminder(reminder, is_last)
-
-
-def print_section(section: dict, is_last=False) -> None:
-    print('{')
-    print(f'  "Title": "{format_title(section['Title'])}",')
-    print('  "Messages": [')
-    print_reminders(section)
-    print('\r  ]')
-    print('}', end='')
-    print('' if is_last else ',')
 
 
 def create_reminders_file(reminders_file: str):
